84064e46ea311cff2d95fd054841b62e815ac828
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-02 13:31:03 +0100

    fix: risolto snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,18 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = [];
+const zucchineRestanti = [];
+
+zucchine.forEach((lunghezza) => {
+  if (lunghezza.length > 15){
+    zucchineLunghe.push(lunghezza)
+  }else{
+    zucchineRestanti.push(lunghezza)
+  }
+})
+
+console.log(zucchineLunghe);
+console.log(zucchineRestanti);
+
