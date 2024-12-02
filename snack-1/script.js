971384571e92914e2012d45c4a60e4417e994cdc
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-02 13:15:32 +0100

    fit: push della cartella

---
+++ snack-1/script.js
@@ -0,0 +1,14 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const newArray = [];
+
+names.forEach((element) => {
+  
+  newArray.push(element)   
+  
+})
+
+console.log(newArray);
