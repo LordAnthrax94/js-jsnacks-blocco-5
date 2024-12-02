c51aaa3fc0c510c36521ec7487975c39e730157a
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-02 13:44:02 +0100

    fix: risolto snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classe = students.find((studente) => {  
+  return studente.class === '3C';
+})
+
+console.log(classe.class);
